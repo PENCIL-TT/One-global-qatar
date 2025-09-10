db7eebbcffee46b6d7a914ed551ef9f325792efb
PENCIL-TT <hariharantt015@gmail.com>
2025-09-10 15:19:42 +0530

    Delete logitrago react directory

--- logitrago react/src/globals/data/contact-us.js
+++
@@ -1,9 +0,0 @@
-import { global } from "../constants";
-
-export const data = {
-    banner: {
-        background: global.banner + "1.jpg",
-        title: "Contact Us",
-        crumb: "Contact Us"
-    }
-}
--- logitrago react/src/globals/data/services-detail.js
+++
@@ -1,9 +0,0 @@
-import { global } from "../constants";
-
-export const data = {
-    banner: {
-        background: global.banner + "1.jpg",
-        title: "Services Detail",
-        crumb: "Services Detail"
-    }
-}
